4abf6aaa9f14d2c81586cf638c332ae6461fa0da
cdstk <cdstkemail@gmail.com>
2025-05-22 02:42:59 -0500

    1.0.6 Boss Crystal Model extend -> copy EnderCrystal

--- src/main/java/lycanitestweaks/client/renderer/entity/RenderBossSummonCrystal.java
+++ src/main/java/lycanitestweaks/client/renderer/entity/RenderBossSummonCrystal.java
@@ -5,13 +5,17 @@ import lycanitestweaks.entity.item.EntityBossSummonCrystal;
 import net.minecraft.client.model.ModelBase;
 import net.minecraft.client.model.ModelEnderCrystal;
 import net.minecraft.client.renderer.GlStateManager;
-import net.minecraft.client.renderer.entity.RenderEnderCrystal;
+import net.minecraft.client.renderer.culling.ICamera;
+import net.minecraft.client.renderer.entity.Render;
+import net.minecraft.client.renderer.entity.RenderDragon;
 import net.minecraft.client.renderer.entity.RenderManager;
-import net.minecraft.entity.item.EntityEnderCrystal;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.MathHelper;
 
-public class RenderBossSummonCrystal extends RenderEnderCrystal {
+import javax.annotation.Nonnull;
+
+public class RenderBossSummonCrystal extends Render<EntityBossSummonCrystal> {
 
     private static final ResourceLocation BOSS_SUMMON_CRYSTAL_TEXTURES = new ResourceLocation(LycanitesTweaks.MODID,"textures/entity/dungeonbosscrystal/dungeonbosscrystal.png");
     private static final ResourceLocation BOSS_SUMMON_CRYSTAL_TEXTURES_DIAMOND = new ResourceLocation(LycanitesTweaks.MODID,"textures/entity/dungeonbosscrystal/dungeonbosscrystal_diamond.png");
@@ -23,11 +27,11 @@ public class RenderBossSummonCrystal extends RenderEnderCrystal {
 
     public RenderBossSummonCrystal(RenderManager renderManagerIn) {
         super(renderManagerIn);
+        this.shadowSize = 0.5F;
     }
 
     @Override
-    public void doRender(EntityEnderCrystal entity, double x, double y, double z, float entityYaw, float partialTicks){
-        super.doRender(entity, x, y, z, entityYaw, partialTicks);
+    public void doRender(EntityBossSummonCrystal entity, double x, double y, double z, float entityYaw, float partialTicks){
         float f = (float)entity.innerRotation + partialTicks;
         GlStateManager.pushMatrix();
         GlStateManager.translate((float)x, (float)y, (float)z);
@@ -53,18 +57,36 @@ public class RenderBossSummonCrystal extends RenderEnderCrystal {
         }
 
         GlStateManager.popMatrix();
+        BlockPos blockpos = entity.getBeamTarget();
+
+        if (blockpos != null){
+            this.bindTexture(RenderDragon.ENDERCRYSTAL_BEAM_TEXTURES);
+            float f2 = (float)blockpos.getX() + 0.5F;
+            float f3 = (float)blockpos.getY() + 0.5F;
+            float f4 = (float)blockpos.getZ() + 0.5F;
+            double d0 = (double)f2 - entity.posX;
+            double d1 = (double)f3 - entity.posY;
+            double d2 = (double)f4 - entity.posZ;
+            RenderDragon.renderCrystalBeams(x + d0, y - 0.3D + (double)(f1 * 0.4F) + d1, z + d2, partialTicks, (double)f2, (double)f3, (double)f4, entity.innerRotation, entity.posX, entity.posY, entity.posZ);
+        }
+
+        super.doRender(entity, x, y, z, entityYaw, partialTicks);
+    }
+
+    @Nonnull
+    @Override
+    public ResourceLocation getEntityTexture(EntityBossSummonCrystal entity){
+        switch (entity.getVariantType()){
+            case -1: return BOSS_SUMMON_CRYSTAL_TEXTURES_ENCOUNTER;
+            case 0: return BOSS_SUMMON_CRYSTAL_TEXTURES;
+            case 1: return BOSS_SUMMON_CRYSTAL_TEXTURES_DIAMOND;
+            case 2: return BOSS_SUMMON_CRYSTAL_TEXTURES_EMERALD;
+        }
+        return new ResourceLocation("textures/entity/endercrystal/endercrystal.png");
     }
 
     @Override
-    public ResourceLocation getEntityTexture(EntityEnderCrystal entity){
-        if(entity instanceof EntityBossSummonCrystal){
-            switch (((EntityBossSummonCrystal) entity).getVariantType()){
-                case -1: return BOSS_SUMMON_CRYSTAL_TEXTURES_ENCOUNTER;
-                case 0: return BOSS_SUMMON_CRYSTAL_TEXTURES;
-                case 1: return BOSS_SUMMON_CRYSTAL_TEXTURES_DIAMOND;
-                case 2: return BOSS_SUMMON_CRYSTAL_TEXTURES_EMERALD;
-            }
-        }
-        return super.getEntityTexture(entity);
+    public boolean shouldRender(@Nonnull EntityBossSummonCrystal livingEntity, @Nonnull ICamera camera, double camX, double camY, double camZ) {
+        return super.shouldRender(livingEntity, camera, camX, camY, camZ) || livingEntity.getBeamTarget() != null;
     }
 }
