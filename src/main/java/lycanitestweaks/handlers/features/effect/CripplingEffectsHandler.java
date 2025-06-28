f9ec83ef621fa91daa4700534139dff56a9c041a
Nischhelm <161029624+Nischhelm@users.noreply.github.com>
2025-06-28 08:29:00 +0200

    Merge pull request #2
    * TODOs added
    * fixed crippling effects damage
    * integration config
    * server config
    * loot config
    * potion config
    * soulkey config
    * loot funcs/conds
    * streamified config reading
    * quick import optimisation

--- src/main/java/lycanitestweaks/handlers/features/effect/CripplingEffectsHandler.java
+++ src/main/java/lycanitestweaks/handlers/features/effect/CripplingEffectsHandler.java
@@ -3,79 +3,113 @@ package lycanitestweaks.handlers.features.effect;
 import com.lycanitesmobs.core.entity.BaseCreatureEntity;
 import lycanitestweaks.potion.PotionCripplingBase;
 import lycanitestweaks.util.Helpers;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.potion.Potion;
 import net.minecraft.util.DamageSource;
-import net.minecraftforge.event.entity.living.LivingDamageEvent;
-import net.minecraftforge.event.entity.living.LivingHurtEvent;
+import net.minecraft.util.EntityDamageSource;
+import net.minecraft.util.EntityDamageSourceIndirect;
+import net.minecraftforge.event.entity.living.LivingAttackEvent;
 import net.minecraftforge.event.entity.living.PotionEvent;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.fml.common.eventhandler.Event;
 import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
-public class CripplingEffectsHandler {
+import java.util.function.Predicate;
 
-    private static DamageSource source;
-    private static float damage = 0;
+public class CripplingEffectsHandler {
 
     @SubscribeEvent(priority = EventPriority.LOWEST)
     public static void cripplingBuffPurge(PotionEvent.PotionApplicableEvent event) {
         if (event.isCanceled()) return;
         EntityLivingBase entity = event.getEntityLiving();
         if (entity == null || entity.getEntityWorld().isRemote) return;
+        Potion appliedPotion = event.getPotionEffect().getPotion();
 
         // bruh why does Rahovart alone keep failing these checks
         // it is denied here but he still has it
-        if(event.getPotionEffect().getPotion() instanceof PotionCripplingBase){
-            if(entity instanceof EntityPlayer
-                && (((EntityPlayer) entity).isCreative() || ((EntityPlayer) entity).isSpectator()))
+        if (appliedPotion instanceof PotionCripplingBase) {
+            if (entity instanceof EntityPlayer
+                    && (((EntityPlayer) entity).isCreative() || ((EntityPlayer) entity).isSpectator())) {
                 event.setResult(Event.Result.DENY);
-            else if((entity instanceof BaseCreatureEntity && ((BaseCreatureEntity) entity).isBoss()))
+                return;
+            } else if ((entity instanceof BaseCreatureEntity && ((BaseCreatureEntity) entity).isBoss())) {
                 event.setResult(Event.Result.DENY);
-        }
-        for(PotionCripplingBase potion : PotionCripplingBase.instanceSet){
-            if(entity.isPotionActive(potion) && potion.shouldDenyBuffs())
-                if(!event.getPotionEffect().getPotion().isBadEffect()) event.setResult(Event.Result.DENY);
-            if(event.getPotionEffect().getPotion() == potion && potion.shouldRemoveBuffs())
+                return;
+            }
+
+            if (((PotionCripplingBase) appliedPotion).shouldRemoveBuffs()) {
                 Helpers.removeAllPositiveEffects(entity);
+                return;
+            }
         }
+
+        if (!appliedPotion.isBadEffect() && entityHasCripplingPotion(event.getEntityLiving(), PotionCripplingBase::shouldDenyBuffs))
+            event.setResult(Event.Result.DENY);
     }
 
     @SubscribeEvent
     public static void cripplingCooldownDeny(PlayerInteractEvent.RightClickItem event) {
-        if(event.getEntityPlayer() == null) return;
-        for(PotionCripplingBase potion : PotionCripplingBase.instanceSet){
-            if(event.getEntityPlayer().isPotionActive(potion) && potion.getItemCooldown() > 0)
-                event.setCanceled(true);
-        }
+        if (event.getEntityPlayer() == null) return;
+        if (entityHasCripplingPotion(event.getEntityLiving(), p ->
+                p.getItemCooldown() > 0))
+            event.setCanceled(true);
     }
 
     @SubscribeEvent(priority = EventPriority.HIGHEST)
-    public static void cripplingPiercingHurt(LivingHurtEvent event) {
-        if (event.getEntityLiving() == null || event.getEntityLiving().getEntityWorld().isRemote) return;
+    public static void cripplingPiercingAttack(LivingAttackEvent event) {
+        if (event.getEntityLiving() == null || event.getEntityLiving().world.isRemote) return;
+        if (event.getSource().isDamageAbsolute() && event.getSource().isUnblockable()) return; //don't run endless loops
+        if (!entityHasCripplingPotion(event.getEntityLiving(), p ->
+                p.shouldPierceAll() || p.shouldPierceEnvironment() && event.getSource().getTrueSource() == null))
+            return;
 
-        for(PotionCripplingBase potion : PotionCripplingBase.instanceSet) {
-            if(event.getEntityLiving().isPotionActive(potion)){
-                if(potion.shouldPierceAll() || potion.shouldPierceEnvironment() && event.getSource().getTrueSource() == null){
-                    source = event.getSource();
-                    damage = event.getAmount();
-                }
-            }
-        }
+        event.setCanceled(true); //don't run the current attack
+
+        //instead run a new attack
+        DamageSource newSource = copyDamageSource(event.getSource()).setDamageBypassesArmor().setDamageIsAbsolute();
+        event.getEntityLiving().attackEntityFrom(newSource, event.getAmount());
     }
 
-    @SubscribeEvent(priority = EventPriority.LOWEST)
-    public static void cripplingPiercingDamage(LivingDamageEvent event) {
-        if (event.getEntityLiving() == null || event.getEntityLiving().getEntityWorld().isRemote) return;
+    private static DamageSource copyDamageSource(DamageSource source){
+        String damageType = source.damageType; //TODO: could use your own damagetype + deathmsg here, but dmg done to player will skip most SME enchants then (forces "player" or "mob")
+        Entity immSource = source.getImmediateSource();
+        Entity trueSource = source.getTrueSource();
 
-        for(PotionCripplingBase potion : PotionCripplingBase.instanceSet) {
-            if(event.getEntityLiving().isPotionActive(potion)){
-                if(source == event.getSource()) {
-                    event.setAmount(Math.max(damage, event.getAmount()));
-                    damage = 0;
-                }
-            }
-        }
+        DamageSource newSource;
+        if(immSource != null)
+            newSource =  new EntityDamageSourceIndirect(damageType, immSource, trueSource);
+        else if(trueSource != null)
+            newSource =  new EntityDamageSource(damageType, trueSource);
+        else
+            newSource = new DamageSource(damageType);
+
+        if(source.canHarmInCreative())
+            newSource.setDamageAllowedInCreativeMode();
+        if(source.isProjectile())
+            newSource.setProjectile();
+        if(source.isFireDamage())
+            newSource.setFireDamage();
+        if(source.isExplosion())
+            newSource.setExplosion();
+        if(source.isDifficultyScaled())
+            newSource.setDifficultyScaled();
+        if(newSource instanceof EntityDamageSource && source instanceof EntityDamageSource && ((EntityDamageSource) source).getIsThornsDamage())
+            ((EntityDamageSource) newSource).setIsThornsDamage();
+
+        //not copying damage location bc it's not really doable and only exists for checking if player can block it with shield, which we gladly disallow
+        //not copying hunger dmg bc it would require a mixin and i don't care enough about it
+
+        return newSource;
+    }
+
+    /**
+     * runs a check over all PotionCripplingBase that match the given predicate.
+     * If the given entity has any of those, return true, otherwise return false
+     */
+    private static boolean entityHasCripplingPotion(EntityLivingBase entity, Predicate<PotionCripplingBase> predicate){
+        return PotionCripplingBase.instanceSet.stream().filter(predicate).anyMatch(entity::isPotionActive);
     }
 }
